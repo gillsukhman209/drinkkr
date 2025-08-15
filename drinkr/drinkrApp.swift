9adb9a734244d8ffae6ad0799996cbc0bc063cfa
gillsukhman209 <104024595+gillsukhman209@users.noreply.github.com>
2025-08-14 19:57:27 -0700

    Initial Commit

---
+++ drinkr/drinkrApp.swift
@@ -0,0 +1,32 @@
+//
+//  drinkrApp.swift
+//  drinkr
+//
+//  Created by Sukhman Singh on 8/14/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct drinkrApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
